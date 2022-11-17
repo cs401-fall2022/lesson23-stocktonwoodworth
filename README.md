a3c553e2d643a944a4b3191c30418a32fbcb7ff5
Stockton <stocktonwoodwort@u.boisestate.edu>
2022-11-17 16:45:00 -0700

    converted to pug, database add and delete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # github-classroom-blank-starter
+### Run
+- npm install
+- npm start
+- Go to [localhost:3000](http://localhost:3000) in browser
--- js/main.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-/**
- * TODO do something
- */
-function newEntry() {
-    var db = new sqlite3.Database('mydb.sqlite3', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
-    )
-}
